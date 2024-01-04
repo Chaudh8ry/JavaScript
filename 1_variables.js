eb2f390d997f787eeb552408cf34fe0b54b4cdd2
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-01-04 22:35:56 +0530

    first commit

---
+++ 1_variables.js
@@ -0,0 +1,15 @@
+// Js is a "Dynamic Typed Language" - we can change the type of any variable in run time.
+// Language like 'C' is "Static Type Language" - we have to first declare the variable whether it is a Integer,String,Floating Point Number or character etc. | it does not allow to change the type of any variable in runtime.
+// now "let" is used to declare variables in Js earlier "var" was used
+console.log("Hola Amigo, Mucho Gusto");
+
+// Here 'a' is "Identifier" - Name given to a variable
+// '8' is the "Literal" - Actual value stored in a variable
+// combination of Identifier and Literal is Variable
+// *  Identifiers are for naming and distinction, while variables are for storing and manipulating values
+let a=8; // variable 'a' contains value 8
+console.log(a); // print value of 'a'
+
+
+a = "harry" // here variable type changed e.g. of "Dynamic Typed Language"
+console.log(a);

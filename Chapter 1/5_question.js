3529cefa9ea12be037b3ca9fad8a35c035167f22
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-01-13 10:36:06 +0530

    .

--- Chapter 1/5_question.js
+++ Chapter 1/5_question.js
@@ -2,7 +2,7 @@
 let a = "Hello";
 let b = 5;
 let c = a+b;
-console.log(c);
+console.log(c); //Output--> Hello5
 
 // use 'typeof' operator to find the datatype of the string above
 console.log(typeof c);

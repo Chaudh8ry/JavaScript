152de7f334d942a8239e3966a9776da9eefdb5cc
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-01-13 12:10:22 +0530

    .

---
+++ Chapter 2/8_question.js
@@ -0,0 +1,8 @@
+// using Logical opertaor check if age lies b/w 10 and 20
+const age = parseInt(prompt("Enter Your Age: "));
+if(age>=10 && age<=20){
+    console.log("Your Age is between 10 and 20");
+}
+else{
+    console.log("Your Age is doesnt lies between 10 and 20");
+}

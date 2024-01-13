2e0b76cfee158942f88321c8df3f5534d6231d14
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-01-13 12:18:31 +0530

    .

---
+++ Chapter 2/10_question.js
@@ -0,0 +1,3 @@
+//using TERNARY Operator find if you can drive or not
+const age = parseInt(prompt("Enter Your Age: "));
+console.log("You", age>=18? "can Drive" : "cant Drive");
---
+++ Chapter 2/9_question.js
@@ -0,0 +1,8 @@
+// If a Number is Divisible by 2 & 3
+const num = parseInt(prompt("Enter a Number: "));
+if(num%2==0 & num%3==0){
+    console.log(num + " is divisible by 2 & 3");
+}
+else{
+    console.log(num + " is NOT divisible by 2 & 3");
+}

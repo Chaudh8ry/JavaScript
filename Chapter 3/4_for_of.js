934b30912aeb0c258920961315401c5995c06aff
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-01-15 23:38:56 +0530

    .

---
+++ Chapter 3/4_for_of.js
@@ -0,0 +1,15 @@
+// Used to iterate over the values of iterable objects like arrays, strings, maps, etc.
+
+// *Iterating over Strings
+let message = "Hello World!";
+
+for(let char of message){
+    console.log(char); // output : H e l l o  W o r l d !
+}
+
+// *Iterating over Array
+let colours = ["Red","Yellow","Blue"];
+// iterating over each colour and printing it
+for(let rang of colours){
+    console.log(rang); // output: Red Yellow Green
+}

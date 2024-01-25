b3a8f76f5d1433d35129b9b69fcb194883159104
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-01-26 01:23:57 +0530

    .

---
+++ Chapter 3/10_question.js
@@ -0,0 +1,7 @@
+// WAP that prints "Try Again" untill user enters the correct Number
+let a = 5;
+let i; 
+while(i!=a){
+    i = parseInt(prompt("Enter a Number: "));
+}
+console.log("You have entered a Correct Number");

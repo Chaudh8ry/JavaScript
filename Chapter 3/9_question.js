9a44a1451f9bb5f00e9cf047a9da7003deaf7cd0
Vishal Chaudhary <122805308+Chaudh8ry@users.noreply.github.com>
2024-01-26 01:01:41 +0530

    .

---
+++ Chapter 3/9_question.js
@@ -0,0 +1,9 @@
+// Question 1 using For in loop
+let marks = {
+    Vishal: 80,
+    Nitin: 75,
+    Sara: 82
+};
+for(let key in marks){
+    console.log("Marks of " + key + " is = " + marks[key]);
+}
